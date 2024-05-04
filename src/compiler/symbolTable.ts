f41b0f9973af3ba76d75cdef8f3cba32db2b9c92
Tuguldur Odnasan <on.tuguldur@gmail.com>
2024-05-04 19:48:47 -0400

    refactor unnecessary for loop to get item fom map

--- src/compiler/symbolTable.ts
+++ src/compiler/symbolTable.ts
@@ -39,27 +39,14 @@ export class SymbolTable {
     }
 
     getVariable(name: string): VariableInfo | undefined {
-        for (let i = this.scopes.length - 1; i >= 0; i--) {
-            if (this.scopes[i].has(name)) {
-                return this.scopes[i].get(name)
-            }
+        if (this.last().get(name)) {
+            return this.last().get(name)
         }
         return undefined
     }
 
     currentScopeLastIndex(): number {
         return this.scopes[this.scopes.length - 1].size
-    }
-
-    findIndex(name: string): number {
-        for (let i = this.scopes.length - 1; i >= 0; i--) {
-            if (this.scopes[i].has(name)) {
-                const index = this.scopes[i].get(name)
-                if (index === undefined) throw new Error(`Index for variable ${name} is undefined`)
-                return index.index
-            }
-        }
-        throw new Error(`Variable ${name} not found in scope`)
     }
 
     addFunction(name: string, params: binaryen.Type[], returnType: binaryen.Type) {
