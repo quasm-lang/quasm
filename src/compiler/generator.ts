quasm-lang/quasm

commit 4bae9e47770bbe269961b5ab9623d32e96e9df00

refactor: Update symbolTable to use VariableReason enum for variable reasons instead of string

src/compiler/generator.ts
@@ -6,12 +6,11 @@ import * as Ast from '../parser/ast.ts'
 import { TokenType } from '../lexer/token.ts'
 import { DataType } from '../datatype/mod.ts'
 import { getWasmType } from './utils.ts'
-import { FunctionSymbol, StringLiteralSymbol, StructSymbol, SymbolTable, SymbolType, VariableSymbol } from './symbolTable.ts'
+import { FunctionSymbol, StringLiteralSymbol, StructSymbol, SymbolTable, SymbolType, VariableReason, VariableSymbol } from './symbolTable.ts'
 import { SemanticAnalyzer } from './semanticAnalyzer.ts'
 
 export class CodeGenerator {
     private module: binaryen.Module
-    // private symbolTable: SymbolTable
     private semanticAnalyzer: SemanticAnalyzer
     private memoryOffset = 0
     private segment: binaryen.MemorySegment[] = []
@@ -156,7 +155,7 @@ export class CodeGenerator {
         const finalType = this.semanticAnalyzer.visitLetStatement(statement)
 
         const index = this.symbolTable.currentScopeLastIndex()
-        this.symbolTable.define({ type: SymbolType.Variable, name: name.value, dataType: finalType, index, reason: 'declaration' } as VariableSymbol)
+        this.symbolTable.define({ type: SymbolType.Variable, name: name.value, dataType: finalType, index, reason: VariableReason.declaration } as VariableSymbol)
         
         return this.module.local.set(index, initExpr)
     }
@@ -170,7 +169,7 @@ export class CodeGenerator {
 
         for (const [index, param] of func.parameters.entries()) {
             params.push(getWasmType(param.dataType))
-            this.symbolTable.define({ type: SymbolType.Variable, name: param.name.value, dataType: param.dataType, index, reason: 'parameter' } as VariableSymbol)
+            this.symbolTable.define({ type: SymbolType.Variable, name: param.name.value, dataType: param.dataType, index, reason: VariableReason.parameter } as VariableSymbol)
         }
 
         // handle return type
@@ -185,7 +184,7 @@ export class CodeGenerator {
         for (const [_name, value] of this.symbolTable.last().symbols) {
             switch (value.type) {
                 case SymbolType.Variable:
-                    if ((value as VariableSymbol).reason === 'declaration') {
+                    if ((value as VariableSymbol).reason === VariableReason.parameter) {
                         vars.push(getWasmType((value as VariableSymbol).dataType))
                     }
             }

src/compiler/semanticAnalyzer.ts
@@ -1,6 +1,6 @@
 import { AstType } from '../parser/ast.ts'
 import * as Ast from '../parser/ast.ts'
-import { VariableSymbol, SymbolTable, SymbolType, FunctionSymbol } from './symbolTable.ts'
+import { VariableSymbol, SymbolTable, SymbolType, FunctionSymbol, VariableReason } from './symbolTable.ts'
 import { TokenType } from '../lexer/token.ts'
 import { DataType } from '../datatype/mod.ts'
 
@@ -71,7 +71,7 @@ export class SemanticAnalyzer {
     
         // Add function parameters to the symbol table
         for (const param of parameters) {
-            this.symbolTable.define({ type: SymbolType.Variable, name: param.name.value, dataType: param.dataType, index: 0, reason: 'parameter' } as VariableSymbol)
+            this.symbolTable.define({ type: SymbolType.Variable, name: param.name.value, dataType: param.dataType, index: 0, reason: VariableReason.parameter } as VariableSymbol)
         }
     
         // Visit the function body

src/compiler/symbolTable.ts
@@ -12,12 +12,17 @@ export interface Symbol {
     name: string
 }
 
+export enum VariableReason {
+    declaration,
+    parameter
+}
+
 export interface VariableSymbol extends Symbol {
     type: SymbolType.Variable
     dataType: DataType
     instanceOf?: string
     index: number
-    reason: 'declaration' | 'parameter'
+    reason: VariableReason
 }
 
 export interface StructSymbol extends Symbol {

commit 0d35a636766402d76c7efef239ddc8f39ce24770

Fix bug introduced in 4bae9e4, and refactorings

src/compiler/generator.ts
@@ -155,7 +155,13 @@ export class CodeGenerator {
         const finalType = this.semanticAnalyzer.visitLetStatement(statement)
 
         const index = this.symbolTable.currentScopeLastIndex()
-        this.symbolTable.define({ type: SymbolType.Variable, name: name.value, dataType: finalType, index, reason: VariableReason.declaration } as VariableSymbol)
+        this.symbolTable.define({
+            type: SymbolType.Variable,
+            name: name.value,
+            dataType: finalType,
+            index,
+            reason: VariableReason.Declaration
+        } as VariableSymbol)
         
         return this.module.local.set(index, initExpr)
     }
@@ -169,7 +175,13 @@ export class CodeGenerator {
 
         for (const [index, param] of func.parameters.entries()) {
             params.push(getWasmType(param.dataType))
-            this.symbolTable.define({ type: SymbolType.Variable, name: param.name.value, dataType: param.dataType, index, reason: VariableReason.parameter } as VariableSymbol)
+            this.symbolTable.define({
+                type: SymbolType.Variable,
+                name: param.name.value,
+                dataType: param.dataType,
+                index,
+                reason: VariableReason.Parameter
+            } as VariableSymbol)
         }
 
         // handle return type
@@ -184,7 +196,7 @@ export class CodeGenerator {
         for (const [_name, value] of this.symbolTable.last().symbols) {
             switch (value.type) {
                 case SymbolType.Variable:
-                    if ((value as VariableSymbol).reason === VariableReason.parameter) {
+                    if ((value as VariableSymbol).reason === VariableReason.Declaration) {
                         vars.push(getWasmType((value as VariableSymbol).dataType))
                     }
             }
@@ -275,7 +287,6 @@ export class CodeGenerator {
             default:
                 return this.module.call('__print_primitive', [val], binaryen.none)
         }
-
     }
 
     private visitExpressionStatement(statement: Ast.ExpressionStatement): binaryen.ExpressionRef {

src/compiler/semanticAnalyzer.ts
@@ -64,14 +64,20 @@ export class SemanticAnalyzer {
     }
 
     visitFuncStatement(statement: Ast.FuncStatement) {
-        const { name, parameters, returnType, body } = statement
+        const { parameters, returnType /* TODO: validate return type */, body } = statement
     
         // Create a new scope for the function
         this.symbolTable.enterScope()
     
         // Add function parameters to the symbol table
-        for (const param of parameters) {
-            this.symbolTable.define({ type: SymbolType.Variable, name: param.name.value, dataType: param.dataType, index: 0, reason: VariableReason.parameter } as VariableSymbol)
+        for (const [index, param] of parameters.entries()) {
+            this.symbolTable.define({
+                type: SymbolType.Variable,
+                name: param.name.value,
+                dataType: param.dataType,
+                index,
+                reason: VariableReason.Parameter
+            } as VariableSymbol)
         }
     
         // Visit the function body
@@ -210,7 +216,7 @@ export class SemanticAnalyzer {
                 return functionInfo.returnType
             }
             default:
-                throw new Error('Placeholder')
+                throw new Error('Invalid symbol type')
         } 
     }
 }

src/compiler/symbolTable.ts
@@ -1,10 +1,10 @@
 import { DataType } from '../datatype/mod.ts'
 
 export enum SymbolType {
-    Variable,
-    Function,
-    Struct,
-    StringLiteral
+    Variable = 'variable',
+    Function = 'function',
+    Struct = 'struct',
+    StringLiteral = 'stringLiteral'
 }
 
 export interface Symbol {
@@ -13,8 +13,8 @@ export interface Symbol {
 }
 
 export enum VariableReason {
-    declaration,
-    parameter
+    Declaration = 'declaration',
+    Parameter = 'parameter',
 }
 
 export interface VariableSymbol extends Symbol {